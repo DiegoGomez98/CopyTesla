a7af1b9555193e8fb6de3dbc0882e0388f04dbbf
Diego Gomez <diargomezp@gmail.com>
2023-02-14 23:24:04 -0500

    barra de tareas lista con js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { click } from '@testing-library/user-event/dist/click';
+
+const header_sectionRight_button = document.querySelector(".header-sectionRight_button");
+const header_sectionCenter = document.querySelector(".header-sectionCenter");
+const header_sectionRight = document.querySelector(".header-sectionRight");
+
+header_sectionRight_button.addEventListener("click", () => {
+  header_sectionCenter.classList.toggle("header-section_visible")
+  header_sectionRight.classList.toggle("header-section_visible")
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
